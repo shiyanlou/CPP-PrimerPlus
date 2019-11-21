98b95485f7252f52c2cb167b48a6e8e5653901eb
eftales <1479803084@qq.com>
2019-11-21 13:48:33 +0800

    init

--- chapter7/7_9.cpp
+++ chapter7/7_9.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
+#include<cstring>
 using namespace std;
-const int SLEN=30;
-struct student{
-	char fullname[SLEN];
-	char hobby[SLEN];
-	int ooplevel;
+const  int SLEN=30;
+struct  student{
+	char  fullname[SLEN];
+	char  hobby[SLEN];
+	int  ooplevel;
 };
 // getinfo() 有两个参数：
 // 一个是指向 student 结构体数组的第一个元素的指针，
@@ -12,23 +13,23 @@ struct student{
 // 该函数可以获取学生的信息并且将信息存储在数组中。
 // 当数组被填满或者在输入学生姓名是输入了一行空行时，该函数将会被停止，
 // 并返回实际存储的学生数量。
-int getinfo(studentpa[],intn);
+int  getinfo(student pa[],int n);
 // display1() 输入学生结构体，显示结构体的内容。
-void display1(studentst);
+void  display1(student st);
 // display2() 输入学生结构体的地址，显示该结构体的内容。
-void display2(conststudent*ps);
+void  display2(const student*ps);
 // display3() 输入学生结构体数组的第一个元素的地址和数组大小，显示结构体的内容。
-void display3(conststudentpa[],intn);
-int main()
+void  display3(const student pa[],int n);
+int  main()
 {
-	cout<<"Enterclasssize:";
-	int class_size;
+	cout<<"Enter class size: ";
+	int  class_size;
 	cin>>class_size;
 	while(cin.get()!='\n')
-	continue;
-	student*ptr_stu=newstudent[class_size];
-	int entered=getinfo(ptr_stu,class_size);
-	for(inti=0;i<entered;i++)
+	    continue;
+	student*ptr_stu=new student[class_size];
+	int  entered=getinfo(ptr_stu,class_size);
+	for(int i=0;i<entered;i++)
 	{
 		display1(ptr_stu[i]);
 		display2(&ptr_stu[i]);
@@ -36,19 +37,19 @@ int main()
 	display3(ptr_stu,entered);
 	delete[]ptr_stu;
 	cout<<"Done\n";
-	return 0;
+	return  0;
 }
 
-int getinfo(studentpa[],intn)
+int  getinfo(student pa[],int n)
 {
-	int num_array_elem=n;
-	char tmp[SLEN];
-	for(inti=0;i<n;++i)
+	int  num_array_elem=n;
+	char  tmp[SLEN];
+	for(int i=0;i<n;++i)
 	{
-		cout<<"Entername:";
+		cout<<"Enter name: ";
 		cin.getline(tmp,SLEN);
-		boolblank_line=true;
-		for(unsignedj=0;j<strlen(tmp);++j)
+		bool blank_line=true;
+		for(unsigned j=0;j<strlen(tmp);++j) // 因为用户可能先输入空格，再输入有效字符，因此需要逐个判断
 		{
 			if(!isspace(tmp[j]))
 			{
@@ -62,31 +63,31 @@ int getinfo(studentpa[],intn)
 			break;
 		}
 		strcpy(pa[i].fullname,tmp);
-		cout<<"Enterhobby:";
+		cout<<"Enter hobby: ";
 		cin.getline(pa[i].hobby,SLEN);
-		cout<<"Enterooplevel:";
+		cout<<"Enter ooplevel: ";
 		cin>>pa[i].ooplevel;
 		cin.get();
 	}
 	cout<<endl;
-	return num_array_elem;
+	return  num_array_elem;
 }
 
-void display1(studentst)
+void  display1(student st)
 {
 	cout<<st.fullname<<'\t'<<st.hobby<<'\t'<<st.ooplevel<<
 	endl;
 }
 
-void display2(conststudent*ps)
+void  display2(const student*ps)
 {
 	cout<<ps->fullname<<'\t'<<ps->hobby<<'\t'<<ps->ooplevel
 	<<endl;
 }
 
-void display3(conststudentpa[],intn)
+void  display3(const student pa[],int n)
 {
-	for(inti=0;i<n;++i)
+	for(int i=0;i<n;++i)
 	cout<<pa[i].fullname<<'\t'<<pa[i].hobby<<'\t'<<
 	pa[i].ooplevel<<endl;
 }
